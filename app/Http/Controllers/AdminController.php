9d2c90c1811a8c826e624e699b8d2e40af21220e
Oleg Sviridov <olegsv3007@yanex.ru>
2020-06-14 15:18:41 +0000

    update statistics queries

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -9,44 +10,33 @@ class AdminController extends Controller
     public function statistics()
     {
         $data['posts_quantity'] = \App\Post::count();
+
         $data['news_quantity'] = \App\News::count();
-        $data['top_author'] = \DB::table('posts')
-            ->leftJoin('users', 'users.id', '=', 'posts.owner_id')
-            ->select('users.name', \DB::raw('count(*) as total'))
-            ->groupBy('owner_id')
-            ->orderByDesc('total')
+
+        $data['top_author'] = \App\User::withCount('posts')
+            ->orderBy('posts_count', 'desc')
             ->first()
             ->name;
 
-        $data['max_length_post'] = \DB::table('posts')
-            ->select('name', 'id', 'slug', \DB::raw('length(body) as length'))
+        $data['max_length_post'] = \App\Post::select('name', 'id', 'slug', \DB::raw('length(body) as length'))
             ->orderBy('length', 'desc')
             ->first();
 
-        $data['min_length_post'] = \DB::table('posts')
-            ->select('name', 'id', 'slug', \DB::raw('length(body) as length'))
+        $data['min_length_post'] = \App\Post::select('name', 'id', 'slug', \DB::raw('length(body) as length'))
             ->orderBy('length', 'asc')
             ->first();
 
-        $data['avg_posts_per_user'] = \DB::table('posts')
-            ->select('owner_id', \DB::raw('count(id) as qty'))
-            ->groupBy('owner_id')
-            ->pluck('qty')
-            ->avg();
+        $data['avg_posts_per_user'] = \App\User::whereHas('posts')
+            ->withCount('posts')
+            ->get()
+            ->avg('posts_count');
 
-        $data['unstable_post'] = \DB::table('posts')
-            ->rightJoin('post_histories', 'posts.id', '=', 'post_histories.post_id')
-            ->select('posts.id','posts.slug', 'posts.name', \DB::raw('count(posts.id) as qty'))
-            ->groupBy('post_histories.post_id')
-            ->orderBy('qty', 'desc')
+        $data['unstable_post'] = \App\Post::withCount('histories')
+            ->orderBy('histories_count', 'desc')
             ->first();
 
-        $data['most_popular_post'] = \DB::table('posts')
-            ->rightJoin('comments', 'posts.id', '=', 'comments.commentable_id')
-            ->where('comments.commentable_type', \App\Post::class)
-            ->select('posts.id','posts.slug', 'posts.name', \DB::raw('count(posts.id) as qty'))
-            ->groupBy('comments.commentable_id')
-            ->orderBy('qty', 'desc')
+        $data['most_popular_post'] = \App\Post::withCount('comments')
+            ->orderBy('comments_count', 'desc')
             ->first();
 
         return view('admin.statistics', compact('data'));
